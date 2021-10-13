8ce2e5ef1eecaf930be3776154cdb0a3094d6330
Inev367 <irenenev24@gmail.com>
2021-10-13 14:13:47 +0000

    Added test image for card positioning

--- static/js/script.js
+++ static/js/script.js
@@ -9,3 +9,10 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('select').formSelect();
 });
+/*For call to action button */
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'left'
+  });
+});
