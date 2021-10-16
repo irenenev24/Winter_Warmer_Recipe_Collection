36a1dab1f86a50a90a1307988b3d0f3c7eaf32db
Inev367 <irenenev24@gmail.com>
2021-10-16 16:14:48 +0000

    Added categories.html

--- app.py
+++ app.py
@@ -114,6 +114,12 @@ def delete_recipe(recipe_id):
     flash("Recipe Successfully Deleted")
     return redirect(url_for("recipes"))
 
+
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
