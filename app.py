96cd6b86ddbc07a9edfb10752e1a590a7ea6371e
Inev367 <irenenev24@gmail.com>
2021-10-18 13:00:53 +0000

    Added logout route

--- app.py
+++ app.py
@@ -82,7 +82,18 @@ def profile(username):
     # to get the session user's username from DB
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You have successfully logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 @app.route("/add_recipe", methods=["GET", "POST"])
